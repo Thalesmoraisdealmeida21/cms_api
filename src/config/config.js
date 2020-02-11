8911c803f140787115a28e5eed1a24282720d5c4
Thales Morais de Almeida <thales.morais21@gmail.com>
2020-02-11 20:46:09 -0300

    Correção de Bugs

--- src/config/config.js
+++ src/config/config.js
@@ -6,14 +6,6 @@ module.exports = {
     database: "db_cms_api",
     secret: "1C3C7E1694F1E9DAD939399E87E5FFB5DF06B2327CA31B409CB3",
 
-    email: {
-        host: "smtp.office365.com", 
-        port: 587,
-        secure: false, 
-        auth: {
-          user: "thalesmoraisdealmeida@outlook.com", 
-          pass: "tma211097" 
-        }
-    }
+  
 
 }
