618b71d4b3bfe770d99edf68dec4d67660eac7cf
Thales Morais <thales.morais21@gmail.com>
2020-05-19 21:33:41 -0300

    Configurado database

--- src/config/config.js
+++ src/config/config.js
@@ -1,9 +1,9 @@
 module.exports = {
-    host: "localhost",
-    username: "root",
-    password: "mysql", 
+    host: "mysql.thalesmoras.kinghost.net",
+    username: "thalesmo01_add1",
+    password: "ta211097", 
     dialect:  "mysql",
-    database: "db_cms_api",
+    database: "thalesmoras01",
     secret: "1C3C7E1694F1E9DAD939399E87E5FFB5DF06B2327CA31B409CB3",
 
 }
