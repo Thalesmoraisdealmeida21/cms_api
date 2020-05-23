36f8754e869acc34d313771eb42413a429c44014
Thales Morais <thales.morais21@gmail.com>
2020-05-23 14:19:23 -0300

    Adicionado gitignora para o .env

--- src/config/config.js
+++ src/config/config.js
@@ -1,9 +1,9 @@
 module.exports = {
-    host: "mysql.thalesmoras.kinghost.net",
-    username: "thalesmo01_add1",
-    password: "ta211097", 
+    host: "localhost",
+    username: "root",
+    password: "mysql", 
     dialect:  "mysql",
-    database: "thalesmoras01",
+    database: "db_cms_api",
     secret: "1C3C7E1694F1E9DAD939399E87E5FFB5DF06B2327CA31B409CB3",
 
 }
