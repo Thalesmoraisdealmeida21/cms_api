fd845bb7f6a98100ccaee9804d22d495598612e4
Thales Morais <thales.morais21@gmail.com>
2020-06-20 17:00:57 -0300

    Corrigidoenvio do e-mail

--- src/controllers/emailController.js
+++ src/controllers/emailController.js
@@ -1,43 +1,51 @@
 const { sendMail } = require('./../services/email')
 const ConfigEmail = require('./../database/models/ConfigEmail')
+const nodemailer = require('nodemailer')
 
 module.exports = () => {
     return {
-        sendMail: (req, res)=>{
+        sendMail: async (req, res)=>{
             data = {
                 to: req.body.to,
                 from: req.body.from,
                 subject: req.body.subject,
+                name: req.body.name,
+                telephone: req.body.telephone,
+                email: req.body.email,
                 message: req.body.message
             }
 
-     
-            if(sendMail(data.from, data.to, data.subject, data.message)){
-                res.json(data)
-            } else {
-                res.status(200).json({msg: "ocorreu um erro"})
-            }
+            let transporter = nodemailer.createTransport({
+                host: "smtp.office365.com",
+                port: 587,
+                secure: false, // true for 465, false for other ports
+                auth: {
+                  user: "thalesmoraisdealmeida@outlook.com", // generated ethereal user
+                  pass: "thalesmorais211097", // generated ethereal password
+                },
+              });
 
-            
+              let info = await transporter.sendMail({
+                from: 'thalesmoraisdealmeida@outlook.com', // sender address
+                to: "thales.morais21@gmail.com", // list of receivers
+                subject: "Contato Via Site", // Subject line
+                text: "Hello world?", // plain text body
+                html: `
+                <h1>Contato Via Site</h1> <br />
+                <strong>Nome: </strong> ${data.name} <br />
+                <strong>E-mail: </strong> ${data.email}  <br />
+                <strong>telephone: </strong> ${data.telephone}  <br />
+                <strong>Assunto: </strong> ${data.subject}  <br />
+                <strong>Mensagem: </strong> ${data.message}`, // html body
+              });
+         
+           
+              console.log("Message sent: %s", info.messageId);
+              console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
 
-            
-        
-        },
 
-        initConfig: (req, res)=>{
-            const {host, port, secure, user, password} = req.body
-            
-
-            ConfigEmail.create({
-                host: host,
-                port: port,
-                secure: secure,
-                user: user,
-                password: password
-            }).then((emailConfig)=>{
-                res.json(emailConfig)
-            })
-
+              res.json({message: info.messageId})
+              
         },
 
         setConfig: (req, res)=> {
